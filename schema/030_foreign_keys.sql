f29156f098710e92cc5c39136e28df7d9f082612
Tolben <vitek.pasek@gmail.com>
2025-10-22 02:17:27 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,4 +1,4 @@
--- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
+-- Auto-generated from schema-map.psd1 (map@1e83bb6)
 -- table: books
 ALTER TABLE books ADD CONSTRAINT fk_books_author FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE RESTRICT;
 
